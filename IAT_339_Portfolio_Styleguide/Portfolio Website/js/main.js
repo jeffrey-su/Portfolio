7b9dada2d8a50ec0887985e785434290b147ce32
jeffrey-su <137955909+jeffrey-su@users.noreply.github.com>
2024-04-08 00:55:51 -0700

    added gosfu page

--- IAT_339_Portfolio_Styleguide/Portfolio Website/js/main.js
+++ IAT_339_Portfolio_Styleguide/Portfolio Website/js/main.js
@@ -1,5 +1,15 @@
-// let screenDiv = document.getElementById('arrowPosition');
-//
-// document.getElementById('thumb').onclick = function(){
-//   screenDiv.classList.toggle('fade')
-// }
+const cursorSmall = document.querySelector('.small');
+const cursorBig = document.querySelector('.big');
+
+
+const positionElement = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+  cursorBig.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+}
+
+window.addEventListener('mousemove', positionElement)
